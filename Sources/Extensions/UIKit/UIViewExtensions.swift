9ab3475eb6ce124b4153991960cc787e38a10b6d
Mabeple <cyyjc@outlook.com>
2018-10-13 20:01:45 +0800

    add function frame

--- Sources/Extensions/UIKit/UIViewExtensions.swift
+++ Sources/Extensions/UIKit/UIViewExtensions.swift
@@ -10,6 +10,7 @@
 import UIKit
 // MARK: - Properties
 public extension UIView {
+    
     /// Height of view.
     public var height: CGFloat {
         get {
@@ -17,6 +18,84 @@ public extension UIView {
         }
         set {
             frame.size.height = newValue
+        }
+    }
+    
+    /// Width of view.
+    public var width: CGFloat {
+        get {
+            return frame.size.width
+        }
+        set {
+            frame.size.width = newValue
+        }
+    }
+    
+    /// X origin of view.
+    public var x: CGFloat {
+        get {
+            return frame.origin.x
+        }
+        set {
+            frame.origin.x = newValue
+        }
+    }
+    
+    /// Y origin of view.
+    public var y: CGFloat {
+        get {
+            return frame.origin.y
+        }
+        set {
+            frame.origin.y = newValue
+        }
+    }
+    
+    /// Top of View
+    public var top: CGFloat {
+        get {
+            return frame.minY
+        }
+        set {
+            var aframe = frame
+            aframe.origin.y = newValue
+            frame = aframe
+        }
+    }
+    
+    /// Bottom of View
+    public var bottom: CGFloat {
+        get {
+            return frame.maxY
+        }
+        set {
+            var aframe = frame
+            aframe.origin.y = newValue - height
+            frame = aframe
+        }
+    }
+    
+    /// Left of View
+    public var left: CGFloat {
+        get {
+            return frame.minX
+        }
+        set {
+            var aframe = frame
+            aframe.origin.x = newValue
+            frame = aframe
+        }
+    }
+    
+    /// Right of View
+    public var right: CGFloat {
+        get {
+            return frame.maxX
+        }
+        set {
+            var aframe = frame
+            aframe.origin.x = newValue - width
+            frame = aframe
         }
     }
 }
--- Tests/UIKitTests/UIViewTexts.swift
+++ Tests/UIKitTests/UIViewTexts.swift
@@ -20,5 +20,61 @@ final class UIViewExtensionsTests: XCTestCase {
         view.height = 150
         XCTAssertEqual(view.frame.size.height, 150)
     }
+    
+    func testWidth() {
+        let frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        let view = UIView(frame: frame)
+        XCTAssertEqual(view.width, 100)
+        view.width = 150
+        XCTAssertEqual(view.frame.size.width, 150)
+    }
+    
+    func testX() {
+        let frame = CGRect(x: 20, y: 20, width: 100, height: 100)
+        let view = UIView(frame: frame)
+        XCTAssertEqual(view.x, 20)
+        view.x = 10
+        XCTAssertEqual(view.frame.origin.x, 10)
+    }
+    
+    func testY() {
+        let frame = CGRect(x: 20, y: 20, width: 100, height: 100)
+        let view = UIView(frame: frame)
+        XCTAssertEqual(view.y, 20)
+        view.y = 10
+        XCTAssertEqual(view.frame.origin.y, 10)
+    }
+    
+    func testTop() {
+        let frame = CGRect(x: 20, y: 50, width: 100, height: 100)
+        let view = UIView(frame: frame)
+        XCTAssertEqual(view.top, 50)
+        view.top = 20
+        XCTAssertEqual(view.frame.minY, 20)
+    }
+    
+    func testBottom() {
+        let frame = CGRect(x: 20, y: 50, width: 100, height: 100)
+        let view = UIView(frame: frame)
+        XCTAssertEqual(view.bottom, 150)
+        view.bottom = 200
+        XCTAssertEqual(view.frame.maxY, 200)
+    }
+    
+    func testLeft() {
+        let frame = CGRect(x: 20, y: 50, width: 100, height: 100)
+        let view = UIView(frame: frame)
+        XCTAssertEqual(view.left, 20)
+        view.left = 200
+        XCTAssertEqual(view.frame.minX, 200)
+    }
+    
+    func testRight() {
+        let frame = CGRect(x: 20, y: 50, width: 100, height: 100)
+        let view = UIView(frame: frame)
+        XCTAssertEqual(view.right, 120)
+        view.right = 200
+        XCTAssertEqual(view.frame.maxX, 200)
+    }
 }
 #endif
