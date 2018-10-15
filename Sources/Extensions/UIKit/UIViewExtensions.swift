eab4ca71b9077cda6c0f6ee36cfc61bb8268476f
Mabeple <87548701@qq.com>
2018-10-15 10:30:03 +0800

    add uiview funtion

--- Sources/Extensions/UIKit/UIViewExtensions.swift
+++ Sources/Extensions/UIKit/UIViewExtensions.swift
@@ -98,10 +98,176 @@ public extension UIView {
             frame = aframe
         }
     }
+    
+    /// Size of view.
+    public var size: CGSize {
+        get {
+            return frame.size
+        }
+        set {
+            width = newValue.width
+            height = newValue.height
+        }
+    }
+    
+    /// Corner radius of view; also inspectable from Storyboard.
+    @IBInspectable public var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.masksToBounds = true
+            layer.cornerRadius = abs(CGFloat(Int(newValue * 100)) / 100)
+        }
+    }
+    
+    /// Border width of view; also inspectable from Storyboard.
+    @IBInspectable public var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    /// Border color of view; also inspectable from Storyboard.
+    @IBInspectable public var borderColor: UIColor? {
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+        set {
+            guard let color = newValue else {
+                layer.borderColor = nil
+                return
+            }
+            layer.borderColor = color.cgColor
+        }
+    }
+    
+    /// Get view's parent view controller
+    public var parentViewController: UIViewController? {
+        weak var parentResponder: UIResponder? = self
+        while parentResponder != nil {
+            parentResponder = parentResponder!.next
+            if let viewController = parentResponder as? UIViewController {
+                return viewController
+            }
+        }
+        return nil
+    }
+    
+    /// Take screenshot of view (if applicable).
+    public var screenshot: UIImage? {
+        UIGraphicsBeginImageContextWithOptions(layer.frame.size, false, 0)
+        defer {
+            UIGraphicsEndImageContext()
+        }
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        layer.render(in: context)
+        return UIGraphicsGetImageFromCurrentImageContext()
+    }
+    
+    /// Shadow color of view; also inspectable from Storyboard.
+    @IBInspectable public var shadowColor: UIColor? {
+        get {
+            guard let color = layer.shadowColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+        set {
+            layer.shadowColor = newValue?.cgColor
+        }
+    }
+    
+    /// Shadow offset of view; also inspectable from Storyboard.
+    @IBInspectable public var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+    /// Shadow opacity of view; also inspectable from Storyboard.
+    @IBInspectable public var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    /// Shadow radius of view; also inspectable from Storyboard.
+    @IBInspectable public var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
 }
 
 // MARK: - Methods
 public extension UIView {
     
+    /// Recursively find the first responder.
+    public func firstResponder() -> UIView? {
+        var views = [UIView](arrayLiteral: self)
+        var i = 0
+        repeat {
+            let view = views[i]
+            if view.isFirstResponder {
+                return view
+            }
+            views.append(contentsOf: view.subviews)
+            i += 1
+        } while i < views.count
+        return nil
+    }
+    
+    /// Set some or all corners radiuses of view.
+    ///
+    /// - Parameters:
+    ///   - corners: array of corners to change (example: [.bottomLeft, .topRight]).
+    ///   - radius: radius for selected corners.
+    public func roundCorners(_ corners: UIRectCorner, radius: CGFloat) {
+        let maskPath = UIBezierPath(
+            roundedRect: bounds,
+            byRoundingCorners: corners,
+            cornerRadii: CGSize(width: radius, height: radius))
+        
+        let shape = CAShapeLayer()
+        shape.path = maskPath.cgPath
+        layer.mask = shape
+    }
+    
+    /// Add shadow to view.
+    ///
+    /// - Parameters:
+    ///   - color: shadow color (default is #137992).
+    ///   - radius: shadow radius (default is 3).
+    ///   - offset: shadow offset (default is .zero).
+    ///   - opacity: shadow opacity (default is 0.5).
+    public func addShadow(ofColor color: UIColor = UIColor(red: 0.07, green: 0.47, blue: 0.57, alpha: 1.0), radius: CGFloat = 3, offset: CGSize = .zero, opacity: Float = 0.5) {
+        layer.shadowColor = color.cgColor
+        layer.shadowOffset = offset
+        layer.shadowRadius = radius
+        layer.shadowOpacity = opacity
+        layer.masksToBounds = false
+    }
+    
+    /// Load view from nib.
+    ///
+    /// - Parameters:
+    ///   - name: nib name.
+    ///   - bundle: bundle of nib (default is nil).
+    /// - Returns: optional UIView (if applicable).
+    public class func loadFromNib(named name: String, bundle: Bundle? = nil) -> UIView? {
+        return UINib(nibName: name, bundle: bundle).instantiate(withOwner: nil, options: nil)[0] as? UIView
+    }
 }
 #endif
--- Tests/UIKitTests/UIViewTexts.swift
+++ Tests/UIKitTests/UIViewTexts.swift
@@ -76,5 +76,153 @@ final class UIViewExtensionsTests: XCTestCase {
         view.right = 200
         XCTAssertEqual(view.frame.maxX, 200)
     }
+    
+    func testSize() {
+        let frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        let view = UIView(frame: frame)
+        XCTAssertEqual(view.size, view.frame.size)
+        
+        view.size = CGSize(width: 50, height: 50)
+        XCTAssertEqual(view.frame.size.width, 50)
+        XCTAssertEqual(view.frame.size.height, 50)
+    }
+    
+    func testCornerRadius() {
+        let frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        let view = UIView(frame: frame)
+        XCTAssertEqual(view.layer.cornerRadius, 0)
+        
+        view.cornerRadius = 50
+        XCTAssertEqual(view.cornerRadius, 50)
+    }
+    
+    func testBorderWidth() {
+        let frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        let view = UIView(frame: frame)
+        view.borderWidth = 0
+        XCTAssertEqual(view.layer.borderWidth, 0)
+        
+        view.borderWidth = 5
+        XCTAssertEqual(view.borderWidth, 5)
+    }
+    
+    func testBorderColor() {
+        let frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        let view = UIView(frame: frame)
+        view.borderColor = nil
+        XCTAssertNil(view.borderColor)
+        view.borderColor = UIColor.red
+        XCTAssertNotNil(view.layer.borderColor)
+        XCTAssertEqual(view.borderColor!, UIColor.red)
+//        XCTAssertEqual(view.layer.borderColor!.uiColor, UIColor.red)
+    }
+    
+    func testParentViewController() {
+        let viewController = UIViewController()
+        XCTAssertNotNil(viewController.view.parentViewController)
+        XCTAssertEqual(viewController.view.parentViewController, viewController)
+        
+        let frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        let view = UIView(frame: frame)
+        XCTAssertNil(view.parentViewController)
+    }
+    
+    func testScreenshot() {
+        let frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        let view = UIView(frame: frame)
+        view.backgroundColor = .black
+        let screenshot = view.screenshot
+        XCTAssertNotNil(screenshot)
+        
+        let view2 = UIView()
+        XCTAssertNil(view2.screenshot)
+    }
+    
+    func testShadowColor() {
+        let frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        let view = UIView(frame: frame)
+        view.layer.shadowColor = nil
+        XCTAssertNil(view.shadowColor)
+        view.shadowColor = UIColor.orange
+        XCTAssertNotNil(view.layer.shadowColor!)
+        XCTAssertEqual(view.shadowColor, UIColor.orange)
+    }
+    
+    func testShadowOffset() {
+        let frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        let view = UIView(frame: frame)
+        view.layer.shadowOffset = CGSize.zero
+        XCTAssertEqual(view.shadowOffset, CGSize.zero)
+        let size = CGSize(width: 5, height: 5)
+        view.shadowOffset = size
+        XCTAssertEqual(view.layer.shadowOffset, size)
+    }
+    
+    func testShadowOpacity() {
+        let frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        let view = UIView(frame: frame)
+        view.layer.shadowOpacity = 0
+        XCTAssertEqual(view.shadowOpacity, 0)
+        view.shadowOpacity = 0.5
+        XCTAssertEqual(view.layer.shadowOpacity, 0.5)
+    }
+    
+    func testShadowRadius() {
+        let frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        let view = UIView(frame: frame)
+        view.layer.shadowRadius = 0
+        XCTAssertEqual(view.shadowRadius, 0)
+        view.shadowRadius = 0.5
+        XCTAssertEqual(view.layer.shadowRadius, 0.5)
+    }
+    
+    func testFirstResponder() {
+        // When there's no firstResponder
+        XCTAssertNil(UIView().firstResponder())
+        
+        let window = UIWindow()
+        
+        // When self is firstResponder
+        let txtView = UITextField(frame: CGRect.zero)
+        window.addSubview(txtView)
+        txtView.becomeFirstResponder()
+        XCTAssertTrue(txtView.firstResponder() === txtView)
+        
+        // When a subview is firstResponder
+        let superView = UIView()
+        window.addSubview(superView)
+        let subView = UITextField(frame: CGRect.zero)
+        superView.addSubview(subView)
+        subView.becomeFirstResponder()
+        XCTAssertTrue(superView.firstResponder() === subView)
+        
+        // When you have to find recursively
+        XCTAssertTrue(window.firstResponder() === subView)
+        
+    }
+    
+    func testRoundCorners() {
+        let frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        let view = UIView(frame: frame)
+        view.roundCorners([.allCorners], radius: 5.0)
+        
+        let maskPath = UIBezierPath(roundedRect: view.bounds,
+                                    byRoundingCorners: [.allCorners],
+                                    cornerRadii: CGSize(width: 5.0, height: 5.0))
+        let shape = CAShapeLayer()
+        shape.path = maskPath.cgPath
+        XCTAssertEqual(view.layer.mask?.bounds, shape.bounds)
+        XCTAssertEqual(view.layer.mask?.cornerRadius, shape.cornerRadius)
+    }
+    
+    func testAddShadow() {
+        let frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        let view = UIView(frame: frame)
+        view.addShadow(ofColor: .red, radius: 5.0, offset: .zero, opacity: 0.5)
+        XCTAssertEqual(view.shadowColor, UIColor.red)
+        XCTAssertEqual(view.shadowRadius, 5.0)
+        XCTAssertEqual(view.shadowOffset, CGSize.zero)
+        XCTAssertEqual(view.shadowOpacity, 0.5)
+    }
 }
 #endif
